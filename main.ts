9e3599f1fa9c9788c90276c3dba9aa4484764750
Ali-Elouasbi <ali.elouasbi@mths.ca>
2024-09-18 11:02:55 -0400

    Added 2-03

--- main.ts
+++ main.ts
@@ -1,8 +1,11 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
  * Created by: Mr. Coxall
- * Created on: Sep 2020
+ * Created on: Sep 2024
  * This program ...
 */
 
-basic.showString('Hello, World!')
+basic.showString('A rectangle has dimesions 5 cm & 3 cm. The perimeter would be:')
+basic.showString((5+5+3+3).toString())
+basic.showString('A rectangle has dimensions 5 cm & 3 cm. The area would be:')
+basic.showString((5*3).toString())
