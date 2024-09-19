5a33590dd815c46484c0dfdf35f9a2c0c601f0c2
Ali-Elouasbi <ali.elouasbi@mths.ca>
2024-09-19 10:12:56 -0400

    added space

--- main.ts
+++ main.ts
@@ -6,6 +6,6 @@
 */
 
 basic.showString('A rectangle has dimesions 5 cm & 3 cm. The perimeter would be:')
-basic.showString((5+5+3+3).toString())
+basic.showString((5 + 3).toString())
 basic.showString('A rectangle has dimensions 5 cm & 3 cm. The area would be:')
-basic.showString((5*3).toString())
+basic.showString((5 * 3).toString())
